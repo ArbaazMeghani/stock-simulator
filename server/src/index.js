7f5a2c49c0a91bfd604cd41ca6e4b4fefa561e9c
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-19 21:55:35 -0500

    Setup basic express server

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Running Express Server on Port: " + port));
