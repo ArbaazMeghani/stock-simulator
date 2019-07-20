38cdc6b8f5f2de276abc80a672ca879e60b523f0
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-19 22:25:51 -0500

    Update spacing

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,15 @@ let express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query {
-        hello: String
-    }
+  type Query {
+    hello: String
+  }
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => 'Hello World!'
-    }
+  Query: {
+    hello: () => 'Hello World!'
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
