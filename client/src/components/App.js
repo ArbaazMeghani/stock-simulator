8e7c383f41c1114d9757ac59c96468c0e2fb3769
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-25 20:22:34 -0500

    Create basic footer and body component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo'; 
 import Header from './Header';
+import Footer from './Footer';
+import Body from './Header copy';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -11,6 +13,8 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Header />
+      <Body />
+      <Footer />
     </ApolloProvider>
   );
 }
