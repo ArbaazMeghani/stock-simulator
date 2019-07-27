085da21281335b423b7e4a18cdbb6d785998fde9
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-27 13:22:14 -0500

    Add a sample chart to the chart area

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import MainChart from './chartArea/MainChart';
 
 export default class Body extends React.Component {
   render() {
     return (
       <Container fluid>
          <Row>
-          <Col style={{backgroundColor: "red"}}>Left Sidebar</Col>
-          <Col style={{backgroundColor: "green"}} xs={8}>Chart Area</Col>
-          <Col style={{backgroundColor: "blue"}}>Right Sidebar</Col>
+          <Col>Left Sidebar</Col>
+          <Col xs={8}> <MainChart /> </Col>
+          <Col>Right Sidebar</Col>
         </Row>
       </Container>
     );
