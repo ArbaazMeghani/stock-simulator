64a3816154b1bb479af14c8b91388e6f8043b09b
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-27 12:29:59 -0500

    Update our overall layout for the simulator

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 export default class Body extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Body</h1>
-      </div>
+      <Container fluid>
+         <Row>
+          <Col style={{backgroundColor: "red"}}>Left Sidebar</Col>
+          <Col style={{backgroundColor: "green"}} xs={8}>Chart Area</Col>
+          <Col style={{backgroundColor: "blue"}}>Right Sidebar</Col>
+        </Row>
+      </Container>
     );
   }
 }
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>
-        Footer
-      </h1>
-    </div>
+    <Navbar bg="dark" variant="dark" fixed="bottom">
+      <Navbar.Text className="mr-auto">Created by: Arbaaz Meghani</Navbar.Text>
+      <Button variant="outline-light" href="https://github.com/ArbaazMeghani/stock-simulator">Source Code</Button>
+    </Navbar>
   );
 }
