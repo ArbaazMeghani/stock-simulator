dea616e629df651b1f9f888a72b2d67c705b2f26
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-26 19:16:27 -0500

    install react-bootstrap and update our header to be a navbar with a login and sign up button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Header</h1>
-      </div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand className="mr-auto" href="#home">Stock Simulator</Navbar.Brand>
+        <ButtonToolbar>
+          <Button className="mx-2" variant="secondary">Login</Button>
+          <Button className="mx-2" variant="primary">Sign Up!</Button>
+        </ButtonToolbar>
+      </Navbar>
     );
   }
 }
