882ef6ef2f5d66f41609dd8bceec11d3bb9e154f
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-29 19:16:17 -0500

    Update AuthModal to have form element for signing in

--- client/src/components/account/AuthModal.js
+++ client/src/components/account/AuthModal.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const style = {
+  maxWidth: "50%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "10ox"
+}
 
 export default function AuthModal() {
   return (
-    <Modal show={false} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal show={false} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+        <Modal.Title>
+          Sign In
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>
-          Modal Content
-        </p>
+        <Form>
+          <Form.Group style={style}>
+            <Form.Control type="text" placeholder="username"/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control style={style} type="password" placeholder="Password"/>
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button>Close</Button>
+        <Button type="submit">Login</Button>
       </Modal.Footer>
     </Modal>
   );
