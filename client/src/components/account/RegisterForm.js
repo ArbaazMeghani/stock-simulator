c97deb77d23a5ff66342907623683cdfb1085f60
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-30 18:59:15 -0500

    Add form for creating a new account

---
+++ client/src/components/account/RegisterForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const style = {
+  maxWidth: "50%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "10ox"
+}
+
+export default function RegisterForm() {
+  return (
+    <Form>
+      <Form.Group style={style}>
+        <Form.Control type="email" placeholder="email"/>
+      </Form.Group>
+
+      <Form.Group style={style}>
+        <Form.Control type="text" placeholder="username"/>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control style={style} type="password" placeholder="Password"/>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control style={style} type="password" placeholder="Confirm Password"/>
+      </Form.Group>
+    </Form>
+  );
+}
