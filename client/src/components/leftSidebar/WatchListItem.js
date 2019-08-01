f5981bf8e1ef366df53bc9887c82970a368f4cd6
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-01 18:35:22 -0500

    Add handler for watchlist

--- client/src/components/leftSidebar/WatchListItem.js
+++ client/src/components/leftSidebar/WatchListItem.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default function WatchListItem( {symbol} ) {
+  const handleClick = () => {
+    console.log(symbol);
+  };
   return (
-    <div className="mt-2">
+    <div className="mt-2" onClick={handleClick}>
       <h6> {symbol} </h6>
     </div>
   );
