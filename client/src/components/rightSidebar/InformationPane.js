c3198f7499f4d48d1f295b0903c506dd1a59061b
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-04 18:45:19 -0500

    Use symbol from redux store for informationpane

--- client/src/components/rightSidebar/InformationPane.js
+++ client/src/components/rightSidebar/InformationPane.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import InformationValue from './InformationValue';
+import { connect } from 'react-redux';
 
-export default class InformationPane extends React.Component {
+class InformationPane extends React.Component {
   render() {
     return (
       <div className="mt-2">
         <h4> Information </h4>
         <Form className="mt-4">
-          <InformationValue label="Symbol" value="APPL"/>
+          <InformationValue label="Symbol" value={this.props.symbol}/>
           <InformationValue label="Company" value="Apple Inc."/>
           <InformationValue label="Price" value="250.00"/>
           <InformationValue label="Open" value="245.00"/>
@@ -18,3 +19,12 @@ export default class InformationPane extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { simpleReducer } = state;
+  return {
+    symbol: simpleReducer.symbol
+  };
+}
+
+export default connect(mapStateToProps, null)(InformationPane);
