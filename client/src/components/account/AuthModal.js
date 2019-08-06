d05cd4dc5f9b4ccbeeb73c8da618464bb84c3521
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-06 18:20:22 -0500

    Start setting up redux store for authentication and choosing which form to show

--- client/src/components/account/AuthModal.js
+++ client/src/components/account/AuthModal.js
@@ -2,21 +2,32 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+import { connect } from 'react-redux';
 
-export default function AuthModal() {
-  return (
-    <Modal show={false} aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Header closeButton>
-        <Modal.Title>
-          Sign In
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <LoginForm />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button type="submit">Login</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+class AuthModal extends React.Component {
+  render() {
+    return (
+      <Modal show={false} aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Sign In
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LoginForm />
+          <RegisterForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit">Login</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  ...state
+})
+
+export default connect(mapStateToProps, null)(AuthModal);
