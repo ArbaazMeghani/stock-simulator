5d2f449964449039bde7d2a1cbf9a2e315e67f05
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-07 18:54:13 -0500

    Update auth reducer to keep track of our authentication state

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,6 +1,13 @@
-export const authAction = (token) => dispatch => {
+export const updateTokenAction = (token) => dispatch => {
   dispatch({
     type: 'AUTH_TOKEN',
     payload: token
+  });
+}
+
+export const beginAuthAction = (authInfo) => dispatch => {
+  dispatch({
+    type: 'BEING_AUTH',
+    payload: authData
   });
 }
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,24 @@
-export default (state = {token: undefined}, action) => {
+export default (
+    state = {
+      token: undefined,
+      show: false,
+      title: '',
+      formType: 0
+    },
+    action
+  ) => {
   switch (action.type) {
-   case 'AUTH_TOKEN':
-    return {
-     token: action.payload
-    };
-   default:
-    return state;
+    case 'AUTH_TOKEN':
+      return {
+        token: action.payload
+      };
+    case 'BEGIN_AUTH':
+      return {
+        show: action.show,
+        title: action.title,
+        formType: action.formType
+      }
+    default:
+      return state;
   }
 }
