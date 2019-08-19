997aaa486a4fe9afefd9529fdb4858f21ce01057
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-19 18:35:37 -0500

    Show username when the user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import AuthButtons from './account/authentication/AuthButtons';
+import { connect } from 'react-redux';
+import Profile from './account/profile/Profile';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+
+  accountArea() {
+    if(this.props.token) {
+      return <Profile />;
+    }
+    return <AuthButtons />;
+  }
+
   render() {
     return (
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand className="mr-auto">Stock Simulator</Navbar.Brand>
-        <AuthButtons />
+        {this.accountArea()}
       </Navbar>
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { authReducer } = state;
+  return {
+    token: authReducer.token
+  };
+}
+
+export default connect(mapStateToProps, null)(Header);
--- client/src/components/account/profile/Profile.js
+++ client/src/components/account/profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-export default class AuthButtons extends React.Component {
+export default class Profile extends React.Component {
   render() {
     return (
       <Button className="mx-2" variant="secondary"> Username </Button>
