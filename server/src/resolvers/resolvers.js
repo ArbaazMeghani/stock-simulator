318e8a51d5cbf08a46bc87156c25b1c87bf5cd44
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-21 19:02:29 -0500

    Move authentication resolvers into a separate file so we can separate the various resolvers into separate files

--- server/src/resolvers/resolvers.js
+++ server/src/resolvers/resolvers.js
@@ -1,52 +1,6 @@
-let bcrypt = require('bcrypt');
-let jwt = require('jsonwebtoken');
 let lodash = require('lodash');
+let authResolvers = require('./authResolvers');
 
-const resolvers = {
-  Query: {
-    currentUser: (root, args, { user }) => user
-  },
-
-  Mutation : {
-    login: async (root, { username, password }, { usersModel, SECRET }) => {
-      const user = await usersModel.findOne({ username });
-      if(!user) {
-        throw new Error('Invalid username or password');
-      }
-
-      const validUser = await bcrypt.compare(password, user.password);
-      if(!validUser) {
-        throw new Error('Invalid username or password');
-      }
-      
-      const token = jwt.sign(
-        {
-          user: lodash.pick(user, ['_id', 'username', 'watchList'])
-        },
-        SECRET,
-        {
-          expiresIn: '1y'
-        });
-      
-      return token;
-    },
-    signup: async (root, { email, username, password }, { usersModel }) => {
-      const existingUser = await usersModel.findOne().or([ { email }, {username} ]);
-
-      if (existingUser) {
-        throw new Error('Email or username already in use');
-      }
-      
-      const hash = await bcrypt.hash(password, 10);
-      const user = await new usersModel({
-        email,
-        username,
-        password: hash
-      }).save();
-
-      return user; 
-    },
-  },
-};
+const resolvers = lodash.merge(authResolvers);
 
 module.exports = resolvers;
