e6c0b0fa88f746104456a972f3c3a7f74e1823e5
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-22 18:53:43 -0500

    Can now add items to the users watchlist

--- server/src/resolvers/watchListResolvers.js
+++ server/src/resolvers/watchListResolvers.js
@@ -1,11 +1,19 @@
 const watchListResolvers = {
   Mutation: {
-    addToWatchList: async (root, { symbol }, { user }) => {
+    addToWatchList: async (root, { symbol }, { usersModel, user }) => {
+      const existingUser = await usersModel.findOne({username: user.username});
+      if(!existingUser) {
+        throw new Error("User not found");
+      }
+      existingUser.watchList.push(symbol);
+      existingUser.save();
       return true
     },
 
-    removeFromWatchList: async (root, { symbol }, { user }) => {
+    removeFromWatchList: async (root, { symbol }, { usersModel, user }) => {
       return true
     }
   }
 }
+
+module.exports = watchListResolvers
