1a54b0b866372ea7ed6fcc6b6cb5d418c3621d2b
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-22 19:00:42 -0500

    Ability to remove items from the users watchlist.

--- server/src/resolvers/watchListResolvers.js
+++ server/src/resolvers/watchListResolvers.js
@@ -2,15 +2,29 @@ const watchListResolvers = {
   Mutation: {
     addToWatchList: async (root, { symbol }, { usersModel, user }) => {
       const existingUser = await usersModel.findOne({username: user.username});
+
       if(!existingUser) {
         throw new Error("User not found");
       }
+
       existingUser.watchList.push(symbol);
+
       existingUser.save();
       return true
     },
 
     removeFromWatchList: async (root, { symbol }, { usersModel, user }) => {
+      const existingUser = await usersModel.findOne({username: user.username});
+
+      if(!existingUser) {
+        throw new Error("User not found");
+      }
+
+      existingUser.watchList = existingUser.watchList.filter(item => {
+        return item != symbol;
+      });
+
+      existingUser.save();
       return true
     }
   }
