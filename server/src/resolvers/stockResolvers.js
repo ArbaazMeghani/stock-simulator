69f7d324634696221bbc07e6a42a658680f65d27
arbaazmeghani <arbaazmeghani@gmail.com>
2019-08-31 18:13:47 -0500

    implementation for buy stock resolver

--- server/src/resolvers/stockResolvers.js
+++ server/src/resolvers/stockResolvers.js
@@ -1,11 +1,34 @@
 const stockResolvers = {
   Mutation: {
     buyStock: async (root, { symbol, price, quantity }, { usersModel, user }) => {
+      const existingUser = await usersModel.findOne({username: user.username});
 
+      if(!existingUser) {
+        throw new Error("User not found");
+      }
+
+      existingStock = undefined;
+
+      for(let i = 0; i < existingUser.stocks.length; i++) {
+        if(existingUser.stocks[i].symbol === symbol) {
+          existingStock = existingUser.stocks[i];
+        }
+      }
+
+      if(existingStock) {
+        existingStock.price = ((existingUser.price * existingUser.quantity) + (price * quantity))/2;
+        existingStock.quantity += quantity;
+      }
+      else {
+        existingUser.stocks.append({symbol: symbol, price: price, quantity: quantity});
+      }
+
+      existingUser.save();
+      return true;
     },
 
     sellStock: async (root, { symbol, price, quantity }, { usersModel, user }) => {
-      
+
     }
   }
 }
