7dc127fcb6727e97f54057e21c10f72ec3b5ef97
arbaazmeghani <arbaazmeghani@gmail.com>
2019-09-02 11:21:56 -0500

    Update user balance when they buy/sell a stock

--- server/src/resolvers/stockResolvers.js
+++ server/src/resolvers/stockResolvers.js
@@ -7,6 +7,10 @@ const stockResolvers = {
         throw new Error("User not found");
       }
 
+      if(price * quantity > existingUser.balance) {
+        throw new Error("You cannot afford that");
+      }
+
       existingStock = undefined;
 
       for(let i = 0; i < existingUser.stocks.length; i++) {
@@ -50,6 +54,7 @@ const stockResolvers = {
           throw new Error("Cannot sell more than you own");
         }
         else {
+          existingUser.balance += quantity * price;
           existingStock.quantity -= quantity;
           if(existingStock.quantity == 0) {
             existingUser.stocks = existingUser.stocks.filter(stock => {
