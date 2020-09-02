0c0496f09a95d84d40e8b12b34c0786918570844
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-09-02 17:58:53 -0500

    wrap special character

--- client/src/components/account/authentication/LoginForm.js
+++ client/src/components/account/authentication/LoginForm.js
@@ -24,7 +24,7 @@ export default function LoginForm( { switchFormType, setUsername, setPassword } 
       </Form.Group>
 
       <Form.Group style={style}>
-        <a href="placeholder" onClick={switchFormType}> No account? Register instead -> </a>
+        <a href="placeholder" onClick={switchFormType}> No account? Register instead -{">"} </a>
       </Form.Group>
     </Form>
   );
