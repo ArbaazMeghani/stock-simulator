2cf3982c16a0cfc2526140deae7a6c545abe7bfd
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-09-15 18:50:42 -0500

    return historic stock data

--- server/src/resolvers/dataResolvers.js
+++ server/src/resolvers/dataResolvers.js
@@ -5,15 +5,27 @@ const dataResolvers = {
     retrieveStockData: async (root, { symbol }) => {
       historic = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${symbol}&apikey=demo`);
       current = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=demo`);
-      // console.log(historic.data);
+      
       const globalQuote = current.data['Global Quote'];
-      console.log(historic);
+      const historicQuote = historic.data['Monthly Adjusted Time Series']
+
+      let dataPoints = []
+      for (var key of Object.keys(historicQuote)) {
+        const date = key.split('-')
+        dataPoints.push({
+          year: date[0],
+          month: date[1],
+          price: historicQuote[key]['4. close']
+        })
+      }
+
       return {
         symbol: globalQuote['01. symbol'],
         price: globalQuote['05. price'],
         open: globalQuote['02. open'],
         close: globalQuote['08. previous close'],
-        volume: globalQuote['06. volume']
+        volume: globalQuote['06. volume'],
+        data: dataPoints
       }
     }
   }
