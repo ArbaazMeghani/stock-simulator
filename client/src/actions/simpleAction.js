8f4396ceae1e05f488c8501a36ba1f7b1ffb74dd
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-09-16 17:43:18 -0500

    setup redux storage for stock data

--- client/src/actions/simpleAction.js
+++ client/src/actions/simpleAction.js
@@ -3,3 +3,7 @@ import baseAction from './baseAction';
 export const simpleAction = (symbol) => dispatch => {
   baseAction('SET_SYMBOL', symbol, dispatch);
 }
+
+export const updateStockData = (stockData) => dispatch => {
+  baseAction('UPDATE_STOCK_DATA', stockData, dispatch);
+}
--- client/src/reducers/simpleReducer.js
+++ client/src/reducers/simpleReducer.js
@@ -1,9 +1,15 @@
-export default (state = {symbol: "AAPL"}, action) => {
+export default (state = {symbol: "AAPL", data: {}}, action) => {
   switch (action.type) {
    case 'SET_SYMBOL':
     return {
-     symbol: action.payload
+     symbol: action.payload,
+     data: state.data
     };
+   case 'UPDATE_STOCK_DATA':
+     return {
+      symbol: state.symbol,
+      data: action.payload
+     }
    default:
     return state;
   }
